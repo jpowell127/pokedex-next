99aa10f3f705c64eb8d043a0250d413cf200a802
Jonathan Powell <jpowell.cr@gmail.com>
2021-10-27 08:50:26 -0500

    add api call to get pokemons, add basic layout to show pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,68 @@
-import type { NextPage } from 'next';
-import Layout from '../components/Layout';
+import type { GetStaticProps, NextPage } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const Home: NextPage = () => {
+import Layout from '../components/Layout';
+interface PokemonData {
+  results: Array<{ name: string; url: string }>;
+}
+
+interface HomeProps {
+  pokemons: Array<{ name: string; url: string; image: string }>;
+}
+
+const { API_URL, API_LIMIT, IMAGE_HOST } = process.env;
+
+export default function Home({ pokemons }: HomeProps) {
   return (
     <Layout title='Pokedex Next'>
       <h1 className='text-4xl mb-8 text-center'>Pokedex Next</h1>
+      <ul>
+        {pokemons.map((pokeman, index) => (
+          <li key={index}>
+            <Link href={`/pokemon?id=${index + 1}`}>
+              <a className='border p-4 border-gray my-2 capitalize flex items-center text-lg bg-gray-200 rounded-md'>
+                <div className='w-20 h-20 mr-3'>
+                  <Image
+                    src={pokeman.image}
+                    alt={pokeman.name}
+                    width={80}
+                    height={80}
+                  />
+                </div>
+                <span className='mr-2 font-bold'>{index + 1}.</span>
+                {pokeman.name}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
-};
+}
 
-export default Home;
+export const getStaticProps: GetStaticProps = async context => {
+  try {
+    const res = await fetch(`${API_URL}?limit=${API_LIMIT}`);
+    const { results }: PokemonData = await res.json();
+    const pokemons = results.map((result, index) => {
+      const paddedIndex = ('00' + (index + 1)).slice(-3);
+      const image = `${IMAGE_HOST}${paddedIndex}.png`;
+      return { ...result, image };
+    });
+
+    return {
+      props: {
+        pokemons,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      props: {
+        pokemons: [],
+      },
+    };
+  }
+};
